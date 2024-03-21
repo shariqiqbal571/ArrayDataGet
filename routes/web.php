8ca73a78417116faacd4f12fef81efa1e06effa6
Shayan R. <52930363+shayanrao@users.noreply.github.com>
2024-03-21 13:42:16 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('post/index', [PostController::class, 'index'])->name('post.index');
+Route::get('post/create', [PostController::class, 'create'])->name('post.create');
+Route::post('post/store', [PostController::class, 'store'])->name('post.store');
+Route::get('post/edit/{id}', [PostController::class, 'edit'])->name('post.edit');
+Route::post('post/update', [PostController::class, 'update'])->name('post.update');
+Route::get('post/destroy/{id}', [PostController::class, 'destroy'])->name('post.destroy');
